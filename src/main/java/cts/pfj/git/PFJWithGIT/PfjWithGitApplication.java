b390b1a59153badc33c54c76f197cfdc33a27a2c
sethsx <suvodipseth@yahoo.in>
2019-09-05 23:12:25 +0530

    First Full Commit

---
+++ src/main/java/cts/pfj/git/PFJWithGIT/PfjWithGitApplication.java
@@ -0,0 +1,13 @@
+package cts.pfj.git.PFJWithGIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfjWithGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfjWithGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cts/pfj/git/PFJWithGIT/PfjWithGitApplicationTests.java
@@ -0,0 +1,16 @@
+package cts.pfj.git.PFJWithGIT;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PfjWithGitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
